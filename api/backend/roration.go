5495b206ab16be8bfe1b199c2ca8095e83961e82
13developing <108137121+13developing@users.noreply.github.com>
2026-03-16 14:59:53 +0800

    Main (#1)
    * 优化轮播图列表结构
    
    * 列表取值统一输出标准_以轮播图模块举例
    
    * 完成手工位管理全功能_带大家有个整体的认识_全新的思路做敏捷开发
    
    * 管理后台admin的增删改查_加密盐_公共类库_业务逻辑判断
    
    * 完成登录鉴权功能
    
    * 2026/3/16 用户名、密码鉴权
    
    ---------
    
    Co-authored-by: 王中阳 <wangzhongyang@stbz.net>

--- api/backend/roration.go
+++ api/backend/roration.go
@@ -12,7 +12,7 @@ type RotationReq struct {
 }
 
 type RotationRes struct {
-	RotationId int `json:"rotationId"`
+	RotationId int `json:"rotation_id"`
 }
 
 type RotationDeleteReq struct {
@@ -31,11 +31,14 @@ type RotationUpdateReq struct {
 type RotationUpdateRes struct {
 	Id uint `json:"id"`
 }
-type RotationGetListReq struct {
-	g.Meta `path:"/backend/rotation/list" method:"get" tags:"轮播图" summary:"轮播图列表"`
-	Sort   int `json:"sort"   in:"query" dc:"排序类型 默认倒序"`
+type RotationGetListCommonReq struct {
+	g.Meta `path:"/backend/rotation/list" method:"get" tags:"轮播图" summary:"轮播图列表接口"`
+	Sort   int `json:"sort"   in:"query" dc:"排序类型"`
 	CommonPaginationReq
 }
-type RotationGetListRes struct {
-	Data interface{}
+type RotationGetListCommonRes struct {
+	List  interface{} `json:"list" description:"列表"`
+	Page  int         `json:"page" description:"分页码"`
+	Size  int         `json:"size" description:"分页数量"`
+	Total int         `json:"total" description:"数据总数"`
 }
