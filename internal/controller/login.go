67bc65f093b0855ac7007ea2fe14e3dc1efeef29
13developing <2958716828@qq.com>
2026-03-18 15:29:31 +0800

    2026/3/16 jwt的对接

--- internal/controller/login.go
+++ internal/controller/login.go
@@ -7,21 +7,11 @@ import (
 )
 
 // 登录管理
-var Login = cLogin{}
-
 type cLogin struct{}
 
+var Login = cLogin{}
+
 func (a *cLogin) Login(ctx context.Context, req *backend.LoginDoReq) (res *backend.LoginDoRes, err error) {
-	//res = &backend.LoginDoRes{}
-	//err = service.Login().Login(ctx, model.UserLoginInput{
-	//	Name:     req.Name,
-	//	Password: req.Password,
-	//})
-	//if err != nil {
-	//	return
-	//}
-	// 识别并跳转到登录前页面
-	//res.Info = service.Session().GetUser(ctx)
 
 	res = &backend.LoginDoRes{}
 	res.Token, res.Expire = service.Auth().LoginHandler(ctx)
--- internal/logic/login/login.go
+++ internal/logic/login/login.go
@@ -3,7 +3,6 @@ package login
 import (
 	"context"
 	"github.com/gogf/gf/v2/errors/gerror"
-	"github.com/gogf/gf/v2/util/gutil"
 	"goframe-shop-v2/internal/dao"
 	"goframe-shop-v2/internal/model"
 	"goframe-shop-v2/internal/model/entity"
@@ -24,25 +23,28 @@ func New() *sLogin {
 // 执行登录
 func (s *sLogin) Login(ctx context.Context, in model.UserLoginInput) error {
 	//验证账号密码是否正确
-	adminInfo := entity.AdminInfo{}
+	adminInfo := new(entity.AdminInfo)
 	err := dao.AdminInfo.Ctx(ctx).Where("name", in.Name).Scan(&adminInfo)
 	if err != nil {
 		return err
 	}
-	gutil.Dump("加密后密码：", utility.EncryptPassword(in.Name, adminInfo.UserSalt))
 	if utility.EncryptPassword(in.Password, adminInfo.UserSalt) != adminInfo.Password {
 		return gerror.New("账号或者密码不正确")
 	}
-	if err := service.Session().SetUser(ctx, &adminInfo); err != nil {
+	if err := service.Session().SetUser(ctx, adminInfo); err != nil {
 		return err
 	}
 	// 自动更新上线
 	service.BizCtx().SetUser(ctx, &model.ContextUser{
-		Id:      uint(adminInfo.Id),
-		Name:    adminInfo.Name,
-		IsAdmin: uint8(adminInfo.IsAdmin),
+		Id:   uint(adminInfo.Id),
+		Name: adminInfo.Name,
 	})
 	return nil
+}
+
+// 注销
+func (s *sLogin) Logout(ctx context.Context) error {
+	return service.Session().RemoveUser(ctx)
 }
 
 // 注销
